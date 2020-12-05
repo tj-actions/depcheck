e032f2b7f7451f42b2729d7f2ee10cbc06f79698
Tonye Jack <tonye@coherentpath.com>
2020-12-05 09:58:35 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -o pipefail
 
-yarn global add depcheck@$INPUT_VERSION
+yarn global add depcheck@"${INPUT_VERSION}"
 
 OUTPUT=$(depcheck --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" 2>&1) && exit_status=$? || exit_status=$?
 echo "$OUTPUT"
