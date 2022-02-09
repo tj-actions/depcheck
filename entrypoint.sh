bece1fcfae60d832aeb8707cacafe518901ba9a5
Tonye Jack <jtonye@ymail.com>
2022-02-08 19:18:28 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@ set -eo pipefail
 
 npm install -g "depcheck@${INPUT_VERSION}"
 
-OUTPUT=$(depcheck --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" 2>&1) && exit_status=$? || exit_status=$?
-echo "$OUTPUT"
+depcheck --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" 2>&1 && exit_status=$? || exit_status=$?
 
 echo "::set-output name=exit_code::$exit_status"
 
