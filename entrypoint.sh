f12e74af5bff1ac8f444a024c5619f2af681e8ac
Tonye Jack <jtonye@ymail.com>
2022-11-07 16:07:27 -0700

    Update entrypoint.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-npx depcheck@${INPUT_VERSION} --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" 2>&1 && exit_status=$? || exit_status=$?
+npx depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" 2>&1 && exit_status=$? || exit_status=$?
 
 echo "::set-output name=exit_code::$exit_status"
 
