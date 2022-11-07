4a043fde62e09d1d0b838312311636e4d7a33373
Tonye Jack <jtonye@ymail.com>
2022-11-07 16:06:39 -0700

    chore: switch to npx

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,7 @@
 
 set -eo pipefail
 
-npm install -g "depcheck@${INPUT_VERSION}"
-
-depcheck --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" 2>&1 && exit_status=$? || exit_status=$?
+npx depcheck@${INPUT_VERSION} --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" 2>&1 && exit_status=$? || exit_status=$?
 
 echo "::set-output name=exit_code::$exit_status"
 
