d34818552c96dbec88c1eb5443c73d496c85d2fc
Dave Johansen <davejohansen@gmail.com>
2025-03-21 15:10:45 -0600

    Double quote to make linter happy

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 npx depcheck@"${INPUT_VERSION}" --ignores="${INPUT_IGNORES}" "${INPUT_DIR}" 2>&1 && exit_status=$? || exit_status=$?
 
-echo "exit_code=$exit_status" >> $GITHUB_OUTPUT
+echo "exit_code=$exit_status" >> "$GITHUB_OUTPUT"
 
 exit $exit_status
